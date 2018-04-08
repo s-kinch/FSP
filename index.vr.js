66995f449cf1c24579c30b0bcb0773357d161e68
sinclairstudy <sinclairstudy@gmail.com>
2018-04-08 18:22:13 -0400

    better lovelace pic

--- index.vr.js
+++ index.vr.js
@@ -11,30 +11,21 @@ export default class WelcomeToVR extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('turing.jpg')}/>
-        // <Text
-        //   style={{
-        //     // backgroundColor: '#777879',
-        //     fontSize: 0.8,
-        //     fontWeight: '400',
-        //     layoutOrigin: [0.5, 0.5],
-        //     paddingLeft: 0.2,
-        //     paddingRight: 0.2,
-        //     textAlign: 'center',
-        //     textAlignVertical: 'center',
-        //     transform: [{translate: [0, 0, -3]}],
-        //   }}>
-        //   yo
-        // </Text>
-        <VrButton
-          style={{width: 0.7}}
-          onClick={()=>this._onViewClicked()}>
-            <Image style={{width:1, height:1}}
-              source={asset('circle.png')}
-              inset={[0.2,0.2,0.2,0.2]}
-              insetSize={[0.05,0.45,0.55,0.15]} >
-            </Image>
-        </VrButton>
+        <Pano source={asset('lovelace2.jpg')}/>
+        <Text
+          style={{
+            // backgroundColor: '#777879',
+            fontSize: 0.8,
+            fontWeight: '400',
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [{translate: [0, 0, -3]}],
+          }}>
+          yo
+        </Text>
       </View>
     );
   }
