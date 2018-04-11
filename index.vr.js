55ffb7bd5d0b41df4d6eccc3effb0fa2d3310459
sinclairstudy <sinclairstudy@gmail.com>
2018-04-10 22:38:30 -0400

    BUTTons!!!!

--- index.vr.js
+++ index.vr.js
@@ -5,30 +5,77 @@ import {
   Pano,
   Text,
   View,
+  VrButton,
+  Image
 } from 'react-vr';
 
 export default class WelcomeToVR extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      location: 'lovelace',
+      bg: asset('lovelace2.jpg'),
+      buttonTransform: [{translate: [-1, 2, -20]}]
+      }
+  }
+
   render() {
+
+
     return (
       <View>
-        <Pano source={asset('lovelace2.jpg')}/>
-        <Text
+
+        <Pano source={this.state.bg}/>
+
+
+
+
+        <VrButton
+          onClick={()=>this.setState(
+            () => this.state.location === 'lovelace' ? {
+              location: 'turing',
+              bg: asset('turing.jpg'),
+              buttonTransform: [{translate: [-6, 1, -30]}]
+            } : {
+              location: 'lovelace',
+              bg: asset('lovelace2.jpg'),
+              buttonTransform: [{translate: [-1, 2, -20]}]
+            }
+            )}
+        >
+        <Image
           style={{
-            // backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
+            height: 2,
+            width: 2,
             layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          yo
-        </Text>
+            transform: this.state.buttonTransform
+          }}
+          source={asset('fi.png')}
+        />
+        </VrButton>
+
       </View>
-    );
+    )
   }
-};
+}
+
+/*
+toggle() {
+  if (this.state.location === 'lovelace'){
+    this.setState({
+      location: 'turing',
+      bg: asset('turing.jpg'),
+      buttonTransform: [{translate: [-1, 2, -20]}]
+      }
+    )
+  } else if (this.state.location === 'turing'){
+    this.setState({
+      location: 'lovelace',
+      bg: asset('lovelace2.jpg'),
+      buttonTransform: [{translate: [-1, 2, -20]}]
+    })
+  }
+}
+*/
 
 AppRegistry.registerComponent('WelcomeToVR', () => WelcomeToVR);
